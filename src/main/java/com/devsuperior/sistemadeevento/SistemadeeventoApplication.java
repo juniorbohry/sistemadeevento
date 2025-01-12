500d5bcd74dca6f9613ca714c4d85b733f9bc332
juniorbohry <juniorbohry@gmail.com>
2025-01-12 10:17:37 -0300

    Primeiro commit

---
+++ src/main/java/com/devsuperior/sistemadeevento/SistemadeeventoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sistemadeevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadeeventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadeeventoApplication.class, args);
+	}
+
+}
